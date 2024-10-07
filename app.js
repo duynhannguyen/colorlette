7d0ba3532bd894e4cfb7be4ab2c67cf096c02c23
Duy Nhan <duynhannguyenn@gmail.com>
2024-10-07 19:05:20 +0700

    generate color

--- app.js
+++ app.js
@@ -4,12 +4,29 @@ const sliders = document.querySelectorAll('input[type = "range"]');
 const currentHexes = document.querySelectorAll(".color h2");
 
 function generateHex() {
-  const letters = "#0123456789abcdef";
-  let hash = "#";
-  for (let i = 0; i < 6; i++) {
-    hash += letters[Math.floor(Math.random() * 16)];
-  }
-  return hash;
+  const hexColor = chroma.random();
+
+  return hexColor;
 }
 
 let randomHex = generateHex();
+
+function randomColors() {
+  colorDivs.forEach((div, index) => {
+    const hexText = div.children[0];
+    const randomColor = generateHex();
+    div.style.backgroundColor = randomColor;
+    hexText.innerText = randomColor;
+    checkTextContrast(randomColor, hexText);
+  });
+}
+
+randomColors();
+function checkTextContrast(color, text) {
+  const luminance = chroma(color).luminance();
+  if (luminance > 0.5) {
+    text.style.color = "black";
+  } else {
+    text.style.color = "white";
+  }
+}
